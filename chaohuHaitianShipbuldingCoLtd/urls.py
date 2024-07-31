d5c15adaa6d850afd54488c7fc593e05d83064c5
deniskathurima <deniskathurima@gmail.com>
2024-07-31 10:23:07 +0300

    first commit

---
+++ chaohuHaitianShipbuldingCoLtd/urls.py
@@ -0,0 +1,35 @@
+"""
+URL configuration for chaohuHaitianShipbuldingCoLtd project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views as chaohu_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', chaohu_views.home, name='home-url'),
+    path('about1/', chaohu_views.about1, name='about1-url'),
+    path('contact1/', chaohu_views.contact1, name='contact1-url'),
+    path('shipBuilding/', chaohu_views.shipBuilding, name='shipBuilding-url'),
+    path('seaHorse/', chaohu_views.seaHorse, name='seaHorse-url'),
+    path('greenline1/', chaohu_views.greenLine1, name='greenline1-url'),
+    path('finFin/', chaohu_views.finFin, name='finFin-url'),
+    path('darEsSalaam/', chaohu_views.darEsSalaam, name='darEsSalaam-url'),
+    path('barrackBarge/', chaohu_views.barrackBarge, name='barrackBarge-url'),
+    path('10MDahor/', chaohu_views.mDahor, name='10MDahor-url'),
+    path('10MPatrolBoat/', chaohu_views.mPatrolBoat, name='10MPatrolBoat-url'),
+    path('dredging/', chaohu_views.dregering, name='dredging-url'),
+]
---
+++ chaohuHaitianShipbuldingCoLtd/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'index1.html')
+
+
+def about1(request):
+    return render(request, 'about1.html')
+
+
+def contact1(request):
+    return render(request, 'contact1.html')
+
+
+def shipBuilding(request):
+    return render(request, 'shipBuilding.html')
+
+
+def seaHorse(request):
+    return render(request, 'seaHorse.html')
+
+
+def greenLine1(request):
+    return render(request, 'greenLine1.html')
+
+
+def finFin(request):
+    return render(request, 'finFin.html')
+
+
+def darEsSalaam(request):
+    return render(request, 'darEsSalaam.html')
+
+
+def barrackBarge(request):
+    return render(request, 'barrackBarge.html')
+
+
+def mDahor(request):
+    return render(request, '10MDahor.html')
+
+
+def mPatrolBoat(request):
+    return render(request, '10MPatrolBoat.html')
+
+
+def dregering(request):
+    return render(request, 'dredging.html')
